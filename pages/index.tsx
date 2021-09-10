f8de7f54ab85e23f31924f8add591e1f33006639
jsalio <tecno12jorge@gmail.com>
2021-09-10 16:05:29 -0400

    Added semantic ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button } from 'semantic-ui-react'
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Button>Hello</Button>
         </h1>
 
         <p className={styles.description}>
