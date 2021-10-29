b9d538dd66fa03e48340a0f778998fc160797edf
jrodriguez <jrodriguez@novosit.com>
2021-10-29 10:10:03 -0400

    enabling cors

--- pages/api/crm/markascomplete/[id].tsx
+++ pages/api/crm/markascomplete/[id].tsx
@@ -1,14 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { CrmProcessConfig } from '../../../../models/Process';
+import NextCors from 'nextjs-cors';
 import { firebase } from '../../../../utils/firebase-config';
 
 const db = firebase.database();
 const jsTable = "card-info"
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
+
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     const { processId } = req.body;
     const { documentTypeId } = req.body;
     const { documentId } = req.body;
